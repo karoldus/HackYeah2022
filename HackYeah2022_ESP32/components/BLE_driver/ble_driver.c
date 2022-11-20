c781252b11e1f9b66c1f0a2978b1d45623ecf2eb
KrystianMand <krystianmand245@gmail.com>
2022-11-20 10:28:48 +0100

    New BLE_driver

---
+++ HackYeah2022_ESP32/components/BLE_driver/ble_driver.c
@@ -0,0 +1,76 @@
+#include "ble_driver.h"
+
+char *TAG = "BLE Client Scan";
+uint8_t ble_addr_type;
+
+static void print_ble_addr(ble_addr_t addr)
+{
+    printf("Addrress type: %d, address value: ", addr.type);
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", addr.val[i]);
+    }
+    printf("\n");
+}
+
+// BLE event handling
+static int ble_gap_event(struct ble_gap_event *event, void *arg)
+{
+    struct ble_hs_adv_fields fields;
+
+    switch (event->type)
+    {
+    // NimBLE event discovery
+    case BLE_GAP_EVENT_DISC:
+        // ESP_LOGI("GAP", "GAP EVENT DISCOVERY");
+        ble_hs_adv_parse_fields(&fields, event->disc.data, event->disc.length_data);
+        if (fields.name_len > 0)
+        {
+            printf("Name: %.*s\n", fields.name_len, fields.name);
+            print_ble_addr(event->disc.addr);
+        }
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+void ble_app_scan(void)
+{
+    printf("Start scanning ...\n");
+
+    struct ble_gap_disc_params disc_params;
+    disc_params.filter_duplicates = 1;
+    disc_params.passive = 0;
+    disc_params.itvl = 0;
+    disc_params.window = 0;
+    disc_params.filter_policy = 0;
+    disc_params.limited = 0;
+
+    ble_gap_disc(ble_addr_type, BLE_HS_FOREVER, &disc_params, ble_gap_event, NULL);
+}
+
+// The application
+void ble_app_on_sync(void)
+{
+    ble_hs_id_infer_auto(0, &ble_addr_type); // Determines the best address type automatically
+    ble_app_scan();                          
+}
+
+// The infinite task
+void host_task(void *param)
+{
+    nimble_port_run(); // This function will return only when nimble_port_stop() is executed
+}
+
+void ble_driver_start(void)
+{
+    nvs_flash_init();                               // 1 - Initialize NVS flash using
+    esp_nimble_hci_and_controller_init();           // 2 - Initialize ESP controller
+    nimble_port_init();                             // 3 - Initialize the controller stack
+    ble_svc_gap_device_name_set("BLE-Scan-Client"); // 4 - Set device name characteristic
+    ble_svc_gap_init();                             // 4 - Initialize GAP service
+    ble_hs_cfg.sync_cb = ble_app_on_sync;           // 5 - Set application
+    nimble_port_freertos_init(host_task);    
+}
